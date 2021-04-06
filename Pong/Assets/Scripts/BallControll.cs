0d3149d4707ee9b49c7c476b54438dcc40352b69
Aruphy <alanluthfi@gmail.com>
2021-04-06 14:21:10 +0700

    fix gaya random
    untuk ballpush fuction

--- Pong/Assets/Scripts/BallControll.cs
+++ Pong/Assets/Scripts/BallControll.cs
@@ -25,8 +25,6 @@ public class BallControll : MonoBehaviour
 
     void PushBall()
     {
-        // Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
-        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
 
         // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
         float randomDirection = Random.Range(0, 2);
@@ -36,11 +34,11 @@ public class BallControll : MonoBehaviour
         if (randomDirection < 1.0f)
         {
             // Gunakan gaya untuk menggerakkan bola ini.
-            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yInitialForce));
         }
         else
         {
-            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yInitialForce));
         }
     }
     void RestartGame()
